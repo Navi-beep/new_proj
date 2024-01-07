eb03c745b463d0ac5558fb3e23ad771a5d01bc5b
Navi-Beep <estawars30@gmail.com>
2024-01-06 21:00:12 -0600

    added function

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ function cats(arr){
     return newArr
 }
 
+function catsTwo(arr){
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(arr[i]**2)
+    }
+    return newArr
+}
+
+
 document.getElementById("cheese").innerHTML = cats([1,2,3,4,5,6,7,8,9,10])
